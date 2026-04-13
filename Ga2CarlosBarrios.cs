5cc4870ccdb454cc1d7a830a149b79cb25da45a8
2017000060-CarlosBarrios <s.barrios.5cdb@gmail.com>
2026-04-13 12:23:20 -0600

    Add files via upload

---
+++ Ga2CarlosBarrios.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("*******************");
+        Console.WriteLine("Dias de la semana");
+        Console.WriteLine("*******************");
+
+        int dia;
+        Console.WriteLine("Digite un valor entero del 1 al 7");
+        dia=Convert.ToInt32(Console.ReadLine());
+
+        switch (dia)
+        {
+            case 1:
+                Console.WriteLine("lUNES");
+                break;
+            case 2:
+                Console.WriteLine("MARTES");
+                break;
+            case 3:
+                Console.WriteLine("MIERCOLES");
+                break;
+            case 4:
+                Console.WriteLine("JUEVES");
+                break;
+            case 5:
+                Console.WriteLine("VIERNES");
+                break;
+            case 6:
+                Console.WriteLine("SABADO");
+                break;
+            case 7:
+                Console.WriteLine("DOMINGO");
+                break;
+            default:
+                Console.WriteLine("Escriba un valor valido del 1 al 7");
+                break;
+        }
+    }
+}
